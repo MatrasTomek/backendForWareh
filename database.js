49e9a992677241f0cafa4ec378ae407c5a9a21c7
DawidWebb <tomasz.matras@developerweb.pl>
2022-05-11 21:53:47 +0200

    change connection with DB

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -22,4 +22,7 @@ const connection = mysql.createConnection({
 //     return;
 //   }
 // });
+
+// connection.end();
+
 module.exports = connection;
