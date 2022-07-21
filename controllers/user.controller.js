b5f54990489e44cc0e27524a677dfb9f2a45e24e
DawidWebb <tomasz.matras@developerweb.pl>
2022-07-21 11:57:18 +0200

    change methods in user controller: get user, change pass

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -17,12 +17,13 @@ exports.postUser = (req, res, next) => {
 
 	db.query(sqlDoubleUserMail, function (err, data) {
 		if (!err) {
-			if (data.length && uzyt_email === `${login}`) {
+			console.log(data);
+			if (data.length && data[0].uzyt_email === `${login}`) {
 				res.json({
 					status: 404,
 					message: `Użytkownik: ${login}, istnieje w bazie danych.`,
 				});
-			} else if (data.length && uzyt_aktywny === 1) {
+			} else if (data.length && data[0].uzyt_aktywny === 1) {
 				res.json({
 					status: 404,
 					message: `Użytkownik: ${login}, zablokowany.`,
@@ -127,29 +128,25 @@ exports.loginUser = (req, res, next) => {
 
 // GET USER
 exports.getUser = (req, res, next) => {
-	const { login } = req.params;
+	const { id } = req.params;
 
-	const sql = "SELECT * FROM Uzytkownik";
-	const searchedData = [];
+	const sqlGetUserById = `SELECT * FROM Uzytkownik WHERE uzyt_id=${id}`;
 
-	db.query(sql, function (err, data, fields) {
-		if (err) throw err;
-		if (!login) {
-			return searchedData;
+	db.query(sqlGetUserById, function (err, data) {
+		if (!err) {
+			const user = {
+				uzyt_id: data[0].uzyt_id,
+				uzyt_email: data[0].uzyt_email,
+				uzyt_aktywny: data[0].uzyt_aktywny,
+			};
+			res.json({ status: 200, data: user });
+		} else {
+			const error = `errCode:${err.code}, errNo:${err.errno}, ${err.sql}`;
+			addError.dataSetError(error);
+			const errData = helpers.handleErrors();
+			res.json(errData);
+			return;
 		}
-
-		data.forEach((item) => {
-			if (item.uzyt_email === login) {
-				searchedData.push(item);
-				res.json(searchedData);
-			} else {
-				const error = `errCode:${err.code}, errNo:${err.errno}, ${err.sql}`;
-				addError.dataSetError(error);
-				const errData = helpers.handleErrors();
-				res.json(errData);
-				return;
-			}
-		});
 	});
 };
 
@@ -161,13 +158,40 @@ exports.putUser = (req, res, next) => {
 	const md5Hasher = crypto.createHmac("md5", secret);
 	const hash = md5Hasher.update(newPassword).digest("hex");
 
-	const sql = `UPDATE Uzytkownik SET uzyt_haslo = '${hash}' WHERE uzyt_id = ${id}`;
-	db.query(sql, function (err, data, fields) {
+	const sqlChangePass = `UPDATE Uzytkownik SET uzyt_haslo = '${hash}' WHERE uzyt_id = ${id}`;
+	db.query(sqlChangePass, function (err, data, fields) {
 		if (!err) {
 			res.json({
 				status: 200,
 				message: "Hasło zostało zmienione",
 			});
+		} else {
+			const error = `errCode:${err.code}, errNo:${err.errno}, ${err.sql}`;
+			addError.dataSetError(error);
+			const errData = helpers.handleErrors();
+			res.json(errData);
+			return;
+		}
+	});
+};
+
+// LOST PASS
+exports.userLostPassword = (req, res) => {
+	const { login } = req.body;
+
+	const sqlFindUserByEmail = `SELECT * FROM Uzytkownik WHERE uzyt_email='${login}'`;
+	db.query(sqlFindUserByEmail, function (err, data) {
+		if (!err) {
+			if (!data.length) {
+				dataErr = { status: 404, message: `użytkownik ${login} nie istnieje` };
+				res.json(dataErr);
+			} else if (data[0].uzyt_aktywny === 0 || data[0].uzyt_zablokow === 1) {
+				dataErr = { status: 404, message: `użytkownik ${login} nie aktywny lub zablokowany` };
+				res.json(dataErr);
+			} else {
+				data = { status: 200, message: true };
+				res.json(data);
+			}
 		} else {
 			const error = `errCode:${err.code}, errNo:${err.errno}, ${err.sql}`;
 			addError.dataSetError(error);
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const userController = require("../controllers/user.controller");
 const router = express.Router();
 
-router.get("/:login", userController.getUser);
+router.get("/:id", userController.getUser);
 router.post("/", userController.loginUser);
 router.post("/add", userController.postUser);
+router.post("/lost-pass", userController.userLostPassword);
 router.put("/", userController.putUser);
 router.delete("/:id", userController.deleteUser);
 router.use((request, response) => response.status(404).end());
