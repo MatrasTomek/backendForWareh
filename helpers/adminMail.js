253ceb570b9e52ad265fbfe0fa58488bcfd125c8
DawidWebb <tomasz.matras@developerweb.pl>
2022-07-28 22:54:23 +0200

    add metyhod to handle of nodemailer

---
+++ helpers/adminMail.js
@@ -0,0 +1,39 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const nodemailer = require("nodemailer");
+const MAIL_PASS = process.env.MAIL_PASS;
+const MAIL_HOST = process.env.MAIL_HOST;
+
+exports.adminInfo = (adminData) => {
+	const { mailFrom, content } = adminData;
+
+	const selfSignedConfig = {
+		host: MAIL_HOST,
+		port: 465,
+		secure: true, // użwa TLS
+		auth: {
+			user: "automat@twojemagazyny.pl",
+			pass: MAIL_PASS,
+		},
+		tls: {
+			// nie przerywa przy błędnym certyfikacie
+			rejectUnauthorized: false,
+		},
+	};
+	const transport = nodemailer.createTransport(selfSignedConfig);
+
+	transport
+		.sendMail({
+			to: "jacek@itjk.pl",
+			from: "automat@twojemagazyny.pl",
+			subject: `Użytkownik ${content}`,
+			html: `<p>Użytkownik: ${mailFrom} - ${content} </p>`,
+		})
+		.then((data) => {
+			return data;
+		})
+		.catch((err) => {
+			console.log(err);
+			return err;
+		});
+};
